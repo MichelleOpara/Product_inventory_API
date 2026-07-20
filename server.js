3c6a4af43384f2e5c0ea5ca27d9ab295a4bb19e5
Emmanuel Gyamfi <emmanuelgyamfi0241@gmail.com>
2026-07-20 23:09:10 +0000

    Add products data

---
+++ server.js
@@ -0,0 +1,32 @@
+let products = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 3500,
+    quantity: 5
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    price: 1500,
+    quantity: 10
+  },
+  {
+    id: 3,
+    name: "Keyboard",
+    price: 200,
+    quantity: 15
+  },
+  {
+    id: 4,
+    name: "Mouse",
+    price: 100,
+    quantity: 20
+  },
+  {
+    id: 5,
+    name: "Monitor",
+    price: 1200,
+    quantity: 8
+  }
+];
