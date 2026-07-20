0312a725f9aa9dbcf7ee95d7d244d074891e76d2
Idris Shawn <idrisshawn10@gmail.com>
2026-07-20 23:43:58 +0100

    Shawn: initialized Express server and configure product routes

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use("/api/products", routesForProducts);
 
 //starting server
 app.listen(PORT, () =>{
-console.log(`Server is running on http://localhost:${PORT}`);
+console.log(`Server is running on http://localhost:${PORT}/home`);
 });
